7b08c7055fca37928d98d0c57ebae733da576c4d
Matt Newville <newville@cars.uchicago.edu>
2015-05-22 21:56:37 -0500

    improvements to setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,40 @@
-import os.path, platform
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-#bits,foo = platform.architecture()
+#!/usr/bin/env python
 
-#
 # Build the pyd with python setup.py build_ext --inplace
-#
-desc = """This is Keith Brafford's first attempt at 
+desc = """This is Keith Brafford's first attempt at
 a wrapper for the FlyCapture2 C API.
 """
 
-FLYCAPTURESDK = r'C:\Program Files (x86)\Point Grey Research\FlyCapture2'
-FLYCAPTURE2LIBDIR = os.path.join(FLYCAPTURESDK, 'lib')
+from setuptools import setup, Extension
+from Cython.Distutils import build_ext
 
-setup(
-    name = 'pyfly2',
-    version = '0.1',
-    description = desc,
-    cmdclass = {'build_ext': build_ext},
-    data_files = ["FlyCapture2_Cd_v90.dll",
-                  "FlyCapture2d_v90.dll", 
-                  "libiomp5md.dll",
-                  ],
-                  
-    #packages = [''],
-    ext_modules = [Extension("pyfly2",
-                             ["pyfly2.pyx"],
-                             include_dirs = [".",
-                                             os.path.join(FLYCAPTURESDK,'include'),
-                                             ],
-                             extra_compile_args = [],
-                             extra_link_args = [],
-                             libraries = ['FlyCapture2_Cd_v90'],
-                             library_dirs = [os.path.join(FLYCAPTURE2LIBDIR, 'C')]
-                             )
-                   ],
-    )
+if os.path.exists('C:/Program Files (x86)/Point Grey Research/FlyCapture2'):
+    fc2_sdk = 'C:/Program Files (x86)/Point Grey Research/FlyCapture2'
+else:
+1    fc2_sdk = 'C:/Program Files/Point Grey Research/FlyCapture2'
 
+if os.path.exists(os.path.join(fc2_sdk, '/lib/C')):
+    fc2_lib = '/lib/C'
+elif os.path.exists(os.path.join(fc2_sdk, '/lib64/C')):
+    fc2_lib = '/lib64/C'
 
+fc2_inc = os.path.join(fc2_sdk, 'include')
+
+setup(name = 'pyfly2',
+      version = '0.2',
+      author = 'Keith Braddord',
+      license = 'BSD',
+      description = desc,
+      cmdclass = {'build_ext': build_ext},
+
+      ext_modules = [Extension('pyfly2',
+                               sources = ['pyfly2.pyx'],
+                               include_dirs = ['.', fc2_inc],
+                               extra_compile_args = [],
+                               extra_link_args = [],
+                               libraries = ['FlyCapture2_Cd_v90'],
+                               library_dirs = [f2c_lib])],
+
+      data_files = ['FlyCapture2_Cd_v90.dll', 'FlyCapture2d_v90.dll',
+                    'libiomp5md.dll'],
+      )
