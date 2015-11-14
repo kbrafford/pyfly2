c960c1a7da58b61e8123d1b2cbf270bcf62a6786
eudoxos <eu@doxos.eu>
2015-11-14 11:38:24 +0100

    Make setup.py work under Linux

--- setup.py
+++ setup.py
@@ -15,18 +15,25 @@ desc = """This is Keith Brafford's first attempt at
 a wrapper for the FlyCapture2 C API.
 """
 
-
-if os.path.exists('C:/Program Files (x86)/Point Grey Research/FlyCapture2'):
-    fc2_sdk = 'C:/Program Files (x86)/Point Grey Research/FlyCapture2'
+if os.name=='posix':
+    fc2_inc='/usr/include/flycapture'
+    fc2_shared='flycapture-c'
+    fc2_lib='/usr/lib'
+    data_files=[]
 else:
-    fc2_sdk = 'C:/Program Files/Point Grey Research/FlyCapture2'
+    # Windows
+    if os.path.exists('C:/Program Files (x86)/Point Grey Research/FlyCapture2'):
+        fc2_sdk = 'C:/Program Files (x86)/Point Grey Research/FlyCapture2'
+    else:
+        fc2_sdk = 'C:/Program Files/Point Grey Research/FlyCapture2'
 
-if os.path.exists(os.path.join(fc2_sdk, 'lib/C')):
-    fc2_lib = os.path.join(fc2_sdk, 'lib/C')
-elif os.path.exists(os.path.join(fc2_sdk, 'lib64/C')):
-    fc2_lib = os.path.join(fc2_sdk, 'lib64/C')
-
-fc2_inc = os.path.join(fc2_sdk, 'include')
+    if os.path.exists(os.path.join(fc2_sdk, 'lib/C')):
+        fc2_lib = os.path.join(fc2_sdk, 'lib/C')
+    elif os.path.exists(os.path.join(fc2_sdk, 'lib64/C')):
+        fc2_lib = os.path.join(fc2_sdk, 'lib64/C')
+    fc2_shared='FlyCapture2_Cd_v90'
+    data_files=['FlyCapture2_Cd_v90.dll', 'FlyCapture2d_v90.dll', 'libiomp5md.dll']
+    fc2_inc = os.path.join(fc2_sdk, 'include')
 
 setup(name = 'pyfly2',
       version = '0.2',
@@ -38,9 +45,7 @@ setup(name = 'pyfly2',
                                include_dirs = ['.', fc2_inc],
                                extra_compile_args = [],
                                extra_link_args = [],
-                               libraries = ['FlyCapture2_Cd_v90'],
+                               libraries = [fc2_shared],
                                library_dirs = [fc2_lib])],
-
-      data_files = ['FlyCapture2_Cd_v90.dll', 'FlyCapture2d_v90.dll',
-                    'libiomp5md.dll'],
+                               data_files = data_files,
       )
